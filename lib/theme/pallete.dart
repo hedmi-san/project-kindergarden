17b11f9a56307b3eabd5763d6a02c7cab1520142
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-09-29 22:23:45 +0100

    Initial commit

---
+++ lib/theme/pallete.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color primarygreenColor = Color.fromRGBO(22, 163, 74, 1);
+  static const Color bordergreyColor = Color.fromRGBO(215, 215, 215, 100);
+  static const Color disablegreyColor = Color.fromRGBO(119, 127, 130, 100);
+  static const Color darkestshadeColor = Color.fromRGBO(38, 50, 56, 100);
+}

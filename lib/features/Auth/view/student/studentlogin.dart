6ff7c0b704ca7b9c3d768ad180ee7c2951c979c3
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-10-01 20:25:50 +0100

    Update student login page

--- lib/features/Auth/view/student/studentlogin.dart
+++ lib/features/Auth/view/student/studentlogin.dart
@@ -7,17 +7,46 @@ class StudentLoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.white,
+        body: Column(
+          children: [
+            Container(
+              height: MediaQuery.sizeOf(context).height * 0.31,
+              width: MediaQuery.sizeOf(context).width,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/student login background image.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Stack(
+                children: [
+                  const Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      'Login étudiant',
+                      style: TextStyle(color: Colors.white, fontSize: 29),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 29,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.sizeOf(context).height * 0.19,
+                  ),
+                  Container(),
+                ],
+              ),
             ),
-            onPressed: () {},
-          ),
-          title: const Text('Login étudiant'),
-          backgroundColor: Colors.transparent,
+          ],
         ),
       ),
     );
@@ -34,5 +63,4 @@ class StudentLoginScreen extends StatelessWidget {
  *    text
  *    tex input 
  *    buttons 
- * 
  */
